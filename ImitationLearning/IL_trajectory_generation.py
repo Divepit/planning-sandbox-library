f4e464e5d95febb153ed0ecd462cda743e1517c8
marcotr <marcotr@ethz.ch>
2024-10-14 13:53:40 +0200

    trajectory generation

--- ImitationLearning/IL_trajectory_generation.py
+++ ImitationLearning/IL_trajectory_generation.py
@@ -3,11 +3,14 @@ import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import logging
 import time
+import gymnasium as gym
 
 from planning_sandbox.environment_class import Environment
 from IL_env import ILEnv
 from stable_baselines3.common.env_checker import check_env
+from stable_baselines3.common.evaluation import evaluate_policy
 from imitation.data.types import Trajectory
+from imitation.algorithms import sqil
 
 
 from copy import deepcopy
@@ -34,31 +37,51 @@ def test_env(env):
 
 def main():
     
-    iterations = 50
     num_goals: int = 6
     num_agents: int = 3
     size: int = 100
     num_skills: int = 2
     solve_type = 'optimal'
     
+    amount_of_trajectories = 50
     
-    logging.info("Setting up environment...")
-    env = Environment(size=size, num_agents=num_agents, num_goals=num_goals, num_skills=num_skills, use_geo_data=True)
-    gym_env = ILEnv(sandboxEnv=env)
-    check_env(gym_env, warn=True)
-    test_env(env)
-
-    # seed_envs = clone_environments(env, iterations)
-
-
     
-    # for env in seed_envs:
-    #     print(f"Progress: {100*(seed_envs.index(env))/iterations:.2f}%", end='\r')
-    #     env_copy: Environment = deepcopy(env)
-    #     env_copy.solve_type = solve_type
-    #     env_copy.find_numerical_solution()
-    #     observation_vector = env_copy.get_observation_vector()
-    #     action_vector = env_copy.get_action_vector()
+    logging.info("Setting up and testing environment...")
+    env_for_test = Environment(size=size, num_agents=num_agents, num_goals=num_goals, num_skills=num_skills, use_geo_data=True, solve_type=solve_type)
+    env_for_check = ILEnv(sandboxEnv=env_for_test)
+    test_env(env_for_test)
+    check_env(env_for_check, warn=True)
+
+    logging.info("Generating trajectories...")
+    trajectories = []
+    sandboxEnv = Environment(size=size, num_agents=num_agents, num_goals=num_goals, num_skills=num_skills, use_geo_data=True, solve_type=solve_type)
+    for _ in range(amount_of_trajectories):
+        print(f"Progress: {len(trajectories)+1}/{amount_of_trajectories}", end='\r')
+        sandboxEnv.find_numerical_solution(solve_type=solve_type)
+        new_trajectory = Trajectory(
+            obs=sandboxEnv.get_observation(),
+            acts=sandboxEnv.get_action_vector(),
+            infos={},
+            terminal=True
+        )
+        trajectories.append(new_trajectory)
+        sandboxEnv.reset()
+
+
+    sqil_trainer = sqil.SQIL(
+        venv=sandboxEnv,
+        demonstrations=trajectories,
+        policy="MlpPolicy",
+    )
+
+    reward_before_training, _ = evaluate_policy(sqil_trainer.policy, sandboxEnv, 10)
+    logging.info(f"Reward before training: {reward_before_training}")
+
+    sqil_trainer.train(
+    total_timesteps=1_000,
+    )  # Note: set to 1_000_000 to obtain good results
+    reward_after_training, _ = evaluate_policy(sqil_trainer.policy, sandboxEnv, 10)
+    logging.info(f"Reward after training: {reward_after_training}")
     
 
 if __name__ == "__main__":
